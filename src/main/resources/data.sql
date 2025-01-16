c1b9a3684f139fedc3ff4293fe2a2a0cbb5dcd1d
dev40018 <g87942131@gmail.com>
2025-01-16 17:21:22 +0330

    tests are completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO mytable (id, name) VALUES
+(1, 'A'),
+(2, 'B'),
+(3, 'C'),
+(4, 'D');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS "mytable";
+
+CREATE TABLE "mytable" (
+    "id" bigint not null,
+    "name" text
+);
